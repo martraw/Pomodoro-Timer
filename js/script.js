62f18095ac30087617c7956fe1cce6ac4de6d1e9
martraw <trawinski1@wp.pl>
2017-06-25 11:24:50 +0200

    Add fadeIn and fadeOut effects to click events in settings

--- js/script.js
+++ js/script.js
@@ -9,55 +9,84 @@ $(function(){
 
     let session = $("#sessionTime"),
         shortBreak = $("#shortBreakTime"),
-        longBreak = $("#longBreakTime");
+        longBreak = $("#longBreakTime"),
+        progressBarTime = $("#progress-bar-time");
 
 
     //Time settings display
     session.text(sessionTime);
     shortBreak.text(shortBreakTime);
     longBreak.text(longBreakTime);
+    progressBarTime.text(sessionTime + " : 00");
 
     //Session time add and substract buttons functionality
+
     $("#sessionTimeAdd").on("click", () => {
-        sessionTime++;
-        session.text(sessionTime);
-    })
+        $(session.text(sessionTime)).fadeOut(100, () => {
+            sessionTime++;
+            $(session.text(sessionTime)).fadeIn(100);
+        });
+    });
+
+    // $("#sessionTimeAdd").on("click", () => {
+    //     sessionTime++;
+    //     session.text(sessionTime);
+    //
+    // })
 
     $("#sessionTimeSub").on("click", () => {
-        sessionTime--;
-        if (sessionTime < 1) {
-            sessionTime = 1;
-        }
-        session.text(sessionTime);
-    })
+        $(session.text(sessionTime)).fadeOut(100, () => {
+            sessionTime--;
+            if (sessionTime < 1) {
+                sessionTime = 1;
+            }
+            $(session.text(sessionTime)).fadeIn(100);
+        });
+    });
+
+    // $("#sessionTimeSub").on("click", () => {
+    //     sessionTime--;
+    //     if (sessionTime < 1) {
+    //         sessionTime = 1;
+    //     }
+    //     session.text(sessionTime);
+    // })
 
     //Short break time add and substract buttons functionality
     $("#shortBreakTimeAdd").on("click", () => {
-        shortBreakTime++;
-        shortBreak.text(shortBreakTime);
-    })
+        $(shortBreak.text(shortBreakTime)).fadeOut(100, () => {
+            shortBreakTime++;
+            $(shortBreak.text(shortBreakTime)).fadeIn(100);
+        });
+    });
 
     $("#shortBreakTimeSub").on("click", () => {
-        shortBreakTime--;
-        if (shortBreakTime < 1) {
-            shortBreakTime = 1;
-        }
-        shortBreak.text(shortBreakTime);
+        $(shortBreak.text(shortBreakTime)).fadeOut(100, () => {
+            shortBreakTime--;
+            if (shortBreakTime < 1) {
+                shortBreakTime = 1;
+            }
+            $(shortBreak.text(shortBreakTime)).fadeIn(100);
+        })
     })
 
     //Long break time add and substract buttons functionality
     $("#longBreakTimeAdd").on("click", () => {
-        longBreakTime++;
-        longBreak.text(longBreakTime);
+        $(longBreak.text(longBreakTime)).fadeOut(100, () => {
+            longBreakTime++;
+            $(longBreak.text(longBreakTime)).fadeIn(100);
+        })
     })
 
     $("#longBreakTimeSub").on("click", () => {
-        longBreakTime--;
-        if (longBreakTime < 1) {
-            longBreakTime = 1;
-        }
-        longBreak.text(longBreakTime);
-    })
+        $(longBreak.text(longBreakTime)).fadeOut(100, () => {
+            longBreakTime--;
+            if (longBreakTime < 1) {
+                longBreakTime = 1;
+            }
+            $(longBreak.text(longBreakTime)).fadeIn(100);
+        });
+    });
 
 
 
