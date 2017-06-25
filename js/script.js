890efe989bce157f6889fb31800fa0e79f222dbb
martraw <trawinski1@wp.pl>
2017-06-25 23:39:54 +0200

    Add main countdown functionality and pause/resume function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(function(){
     // Time settings
-    let sessionTime = 25,
-        shortBreakTime = 5,
+    let sessionTime = 1,
+        shortBreakTime = 2,
         longBreakTime = 15,
         isTimeRunning = false,
+        resumed = false,
+        isSession = true,
         completedSessions = 0,
-        _60sec = 60;
+        seconds = 60,
+        mainInterval,
+        tempMin,
+        tempSec,
+        tempBreakMin,
+        tempBreakSec;
 
+    //UI elements
     let session = $("#sessionTime"),
         shortBreak = $("#shortBreakTime"),
         longBreak = $("#longBreakTime"),
+        progressBar = $(".main-bar"),
+        progressBarWidth = 100,
         progressBarTime = $("#progress-bar-time");
 
 
@@ -20,19 +30,12 @@ $(function(){
     progressBarTime.text(sessionTime + " : 00");
 
     //Session time add and substract buttons functionality
-
     $("#sessionTimeAdd").on("click", () => {
         $(session.text(sessionTime)).fadeOut(100, () => {
             sessionTime++;
             $(session.text(sessionTime)).fadeIn(100);
         });
     });
-
-    // $("#sessionTimeAdd").on("click", () => {
-    //     sessionTime++;
-    //     session.text(sessionTime);
-    //
-    // })
 
     $("#sessionTimeSub").on("click", () => {
         $(session.text(sessionTime)).fadeOut(100, () => {
@@ -43,14 +46,6 @@ $(function(){
             $(session.text(sessionTime)).fadeIn(100);
         });
     });
-
-    // $("#sessionTimeSub").on("click", () => {
-    //     sessionTime--;
-    //     if (sessionTime < 1) {
-    //         sessionTime = 1;
-    //     }
-    //     session.text(sessionTime);
-    // })
 
     //Short break time add and substract buttons functionality
     $("#shortBreakTimeAdd").on("click", () => {
@@ -87,6 +82,123 @@ $(function(){
             $(longBreak.text(longBreakTime)).fadeIn(100);
         });
     });
+
+    progressBar.on("click", () => {
+        startStop();
+    } )
+
+    function timer(){
+        isTimeRunning = true;
+        // let currentMin = sessionTime -1;
+        // let totalSec = sessionTime * 60;
+        let currentMin;
+        let totalSec;
+
+        if (resumed === false && isSession === true) {
+            currentMin = sessionTime -1;
+            totalSec = sessionTime * 60;
+            tempMin = currentMin;
+            tempSec = totalSec;
+            // console.log("Pierwszy if: sessionTime", sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; resumed = ", resumed);
+
+        } else if (resumed === true && isSession === true) {
+            currentMin = tempMin;
+            totalSec = tempSec;
+            // console.log("Pierwszy else if: sessionTime", sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; resumed = ", resumed);
+
+        }   else if (resumed === false && isSession === false) {
+            currentMin = tempBreakMin;
+            totalSec = tempBreakSec;
+            console.log("Drugi else if: sessionTime",  sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; tempBreakMin " + tempBreakMin + "; tempBreakSec ", tempBreakSec +  "; resumed = ", resumed);
+
+        }  else if (resumed === true && isSession === false) {
+            currentMin = tempBreakMin
+            totalSec = tempBreakSec
+            console.log("Trzeci else if: sessionTime", sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; tempBreakMin " + tempBreakMin + "; tempBreakSec ", tempBreakSec +  "; resumed = ", resumed);
+        }
+
+        console.log("currentMin: ",currentMin);
+
+        mainInterval = setInterval(() => {
+            // console.log(progressBarWidth);
+
+            progressBarWidth -= 100/totalSec;
+            progressBar.css("width", progressBarWidth + "%");
+            // console.log("Diałam");
+
+            seconds -= 1;
+            if (seconds < 0) {
+                seconds = 59;
+                // console.log("currentMin:  ",currentMin + " seconds", seconds);
+                currentMin -= 1;
+                if (tempMin > 0) {
+                    tempMin -= 1;
+                }
+                if (tempBreakMin > 0) {
+                    tempBreakMin -=1;
+                }
+
+                console.log("Odliczanie sekund: sessionTime", sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; resumed = ", resumed);
+                // console.log("1 tempMin ", tempMin );
+            }
+
+            // break
+            if (isSession === true) {
+                if (currentMin < 0) {
+                    isSession = false;
+                    resumed = false;
+                    currentMin = shortBreakTime -1;
+                    tempBreakMin = currentMin;
+                    totalSec = shortBreakTime * 60;
+                    tempBreakSec = totalSec
+                    $("#status").text("BREAK");
+                    progressBarWidth = 100;
+                    progressBarWidth -= 100/totalSec;
+                    progressBar.css("width", progressBarWidth + "%");
+
+                    console.log("Przerwa sessionTime", sessionTime + "; currentMin ", currentMin + "; totalSec ", totalSec + "; tempMin " + tempMin + "; tempSec ", tempSec + "; resumed = ", resumed);
+                }
+            }
+
+            //Next session
+            else if (currentMin < 0) {
+                tempMin = 0;
+                tempSec = 0;
+                resumed = false;
+                isSession = true;
+                $("#status").text("Session");
+                currentMin = sessionTime -1;
+                totalSec = sessionTime * 60;
+                tempMin = currentMin;
+                tempSec = totalSec;
+                progressBarWidth = 100;
+                progressBarWidth -= 100/totalSec;
+                progressBar.css("width", progressBarWidth + "%");
+
+            }
+
+
+            if (seconds < 10) {
+                progressBarTime.text(currentMin + " : 0" + seconds);
+            } else {
+                progressBarTime.text(currentMin + " : " + seconds);
+            }
+
+        }, 200);// Koniec Interwału
+    };// Koniec funkcji timer()
+
+    function startStop(){
+        resumed = true
+        if (isTimeRunning === true) {
+            clearInterval(mainInterval);
+            isTimeRunning = false;
+
+        } else {
+            timer();
+        }
+    }
+
+timer()
 
 
 
