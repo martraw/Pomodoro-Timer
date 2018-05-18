e2aeb269065aa4ec31d9c792117080e3c8d32ce4
Marcin Trawiński <trawinski1@wp.pl>
2018-05-18 18:31:11 +0200

    Add ui elements to constructor

---
+++ src/modules/ui.js
@@ -0,0 +1,24 @@
+class UI {
+  constructor() {
+    this.session = document.querySelector('#sessionTime'),
+    this.shortBreak = document.querySelector('#shortBreakTime'),
+    this.longBreak = document.querySelector('#longBreakTime'),
+    this.progressBar = document.querySelector('.mani-bar'),
+    this.progressBarContainer = document.querySelector('#progress-bar-container'),
+    this.progressBarWidth = 100,
+    this.progressBarTime = document.querySelector('#progress-bar-time'),
+    this.descriptionQuestion = document.querySelector('#descriptionQuestion'),
+    this.sound1 = new Audio('./sounds/timeForBreak.ogg'),
+    this.sound2 = new Audio('./sounds/timeToWork.ogg'),
+    this.sound3 = new Audio('./sounds/long_break.mp3')
+    
+  }
+
+  updateUiElement(element, value) {
+    element.textContent = value
+  }
+}
+
+const ui = new UI();
+
+export default ui;
